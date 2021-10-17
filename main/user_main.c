eb03a838e58a37c34ce4b3862a110bc63155baf3
Holger Adams <mail@dm5tt.de>
2021-10-17 19:33:18 +0200

    user_main: add my name to the copyright notice

--- main/user_main.c
+++ main/user_main.c
@@ -10,6 +10,7 @@
 /*
 
 Copyright (c) 2018 Dana H. Myers, K6JQ
+Copyright (c) 2021 Holger Adams, DM5TT
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
